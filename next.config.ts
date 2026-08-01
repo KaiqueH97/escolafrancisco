c68e62211ac115a35dcc73e961659011bc7a0e1b
Kaique Santos <kaiqueh.felix@gmail.com>
2026-08-01 11:51:03 -0300

    feat: configuracao do pwa (manifest e next-pwa)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
